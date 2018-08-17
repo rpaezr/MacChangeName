0f18ab0f75af74e5cbb5d9995df3c7cbd3d84b03
Rolando Paez <rolando@rpaez.com>
2018-08-17 10:33:54 -0500

    modified:   changename.sh Deleted sudo command, added command to refresh jamf

--- changename.sh
+++ changename.sh
@@ -1,12 +1,18 @@
 # Get system Serial Number
 echo "Getting system serial number..."
-#NEWNAME="MacBook"
 NEWNAME="$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')"
+
 echo "New Computer name should be:"
 echo ${NEWNAME}
+
 echo "Changing computer name now..."
-sudo scutil --set HostName ${NEWNAME}
-sudo scutil --set LocalHostName ${NEWNAME}
-sudo scutil --set ComputerName ${NEWNAME}
+
+scutil --set HostName ${NEWNAME}
+scutil --set LocalHostName ${NEWNAME}
+scutil --set ComputerName ${NEWNAME}
 dscacheutil -flushcache
+
+# An inventory status update is performed and sent to JAMF
+jamf recon
+
 echo "All done"
